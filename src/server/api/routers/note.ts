ce67a8bbe81fe5a11e83586971afe98398c0ae75
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-15 13:07:38 +0300

    finalised creation of note router

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -1,3 +1,45 @@
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const noteRouter = createTRPCRouter({
+    create: protectedProcedure
+        .input(
+            z.object({ title: z.string(), content: z.string(), topicId: z.string() })
+        )
+        .mutation(
+            async ({ ctx, input }) => {
+                return ctx.prisma.note.create({
+                    data: {
+                        title: input.title,
+                        topicId: input.topicId,
+                        content: input.content,
+                    }
+                })
+            }
+        ),
+
+    delete: protectedProcedure
+        .input(
+            z.object({ id: z.string() })
+        )
+        .mutation(
+            async ({ ctx, input }) => {
+                return ctx.prisma.note.delete({
+                    where: {
+                        id: input.id
+                    },
+                });
+            }
+        ),
+
+    getAll: protectedProcedure
+        .input(z.object({ topicId: z.string() }))
+        .query(({ ctx, input }) => {
+            return ctx.prisma.note.findMany({
+                where: {
+                    topicId: input.topicId,
+                }
+            });
+        }),
+})
